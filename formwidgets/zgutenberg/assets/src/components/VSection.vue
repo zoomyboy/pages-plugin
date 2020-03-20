36badf98e86d6ccbf97369e1ddee60173330950c
philipp lang <philipp@aweos.de>
2020-03-20 01:58:24 +0100

    Remove console log

--- formwidgets/zgutenberg/assets/src/components/VSection.vue
+++ formwidgets/zgutenberg/assets/src/components/VSection.vue
@@ -43,7 +43,6 @@ export default {
                     };
                 });
             }).catch(err => {
-                console.log(err);
                 this.$emit('destroy');
             });
         }
