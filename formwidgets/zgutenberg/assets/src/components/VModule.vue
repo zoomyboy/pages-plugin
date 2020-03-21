c684a32a9fdc92ef17e54bcffcec1c83e67a1d80
philipp lang <philipp@aweos.de>
2020-03-21 18:10:56 +0100

    Add: Edit module

--- formwidgets/zgutenberg/assets/src/components/VModule.vue
+++ formwidgets/zgutenberg/assets/src/components/VModule.vue
@@ -1,8 +1,7 @@
 <template>
-    <div class="zg-bg-gray-800 zg-rounded zg-p-2 zg-flex zg-items-center">
-        <div class="title w-full">
-            <input type="text" v-model="value.name" class="zg-leading-none zg-bg-gray-800 zg-outline-none zg-text-center zg-text-white zg-w-full zg-border-0">
-        </div>
+    <div class="zg-bg-gray-800 zg-rounded zg-p-2 zg-flex zg-items-center flex">
+        <input type="text" v-model="value.name" class="zg-leading-none zg-bg-gray-800 zg-outline-none zg-text-center zg-text-white zg-w-full zg-border-0 flex-grow">
+        <a href="#" @click.prevent="edit" class="hover:zg-no-underline"><span class="zg-text-white icon-cog"></span></a>
     </div>
 </template>
 
@@ -25,9 +24,21 @@ export default {
 
     components: { VColumn },
 
+    methods: {
+        edit() {
+            this.$store.dispatch('modal/open', {
+                'component': this.component,
+                params: { params: this.value }
+            }).then(data => {
+                this.$emit('input', data);
+            }).catch(err => {
+                    
+            });
+        }
+    },
+
     mounted() {
         if (this.new) {
-            console.log(this.value);
             this.$store.dispatch('modal/open', {
                 'component': this.component,
                 params: { params: this.value }
