1f5811c5c91b20ac91052f8271701a127af40f98
philipp lang <philipp@aweos.de>
2020-03-26 02:22:38 +0100

    Add page title component

---
+++ formwidgets/zgutenberg/assets/src/blocks/Pagetitle.vue
@@ -0,0 +1,29 @@
+<template>
+    <div></div>
+</template>
+
+<script>
+import BlockMixin from '../block.mixin.js';
+import StripMixin from '../strip.mixin.js';
+
+export default {
+
+    props: {
+        value: {}
+    },
+
+    mounted() {
+        this.$emit('confirm', this.value);
+    }
+
+};
+
+const params = {
+    icon: 'header',
+    name: 'Seitentitel',
+    params: {}
+};
+
+export { params };
+</script>
+
