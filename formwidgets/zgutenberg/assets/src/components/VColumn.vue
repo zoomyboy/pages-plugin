292df10222ee441e6d56a0423c60c91c8c381b90
philipp lang <philipp@aweos.de>
2020-03-29 18:12:26 +0200

    Modify modules inside a column as events

--- formwidgets/zgutenberg/assets/src/components/VColumn.vue
+++ formwidgets/zgutenberg/assets/src/components/VColumn.vue
@@ -9,12 +9,12 @@
             <div class="zg-bg-module zg-flex zg-rounded zg-p-3 zg-items-center">
                 <span class="zg-text-gray-600" :class="'icon-'+module.is.icon"></span>
                 <input type="text" v-model="module.meta.title" class="zg-flex-grow zg-border-0 zg-leading-none zg-bg-module zg-outline-none zg-text-center zg-text-white zg-w-full">
-                <a href="#" @click.prevent="edit(module, index)" class="hover:zg-no-underline zg-mr-2"><span class="zg-text-white icon-cog"></span></a>
-                <a href="#" @click.prevent="remove(index)" class="hover:zg-no-underline"><span class="zg-text-white icon-trash"></span></a>
+                <a href="#" @click.prevent="$emit('edit', index)" class="hover:zg-no-underline zg-mr-2"><span class="zg-text-white icon-cog"></span></a>
+                <a href="#" @click.prevent="$emit('delete', index)" class="hover:zg-no-underline"><span class="zg-text-white icon-trash"></span></a>
             </div>
 
             <div class="zg-flex zg-justify-center zg-absolute zg--mt-3 zg-w-full">
-                <a href="#" @click.prevent="addModule(index)" class="zg-btn zg-btn-sm zg-bg-module">
+                <a href="#" @click.prevent="$emit('create', index)" class="zg-btn zg-btn-sm zg-bg-module">
                     <span class="icon-plus"></span>
                 </a>
             </div>
@@ -23,7 +23,7 @@
 
         <div v-if="value.length === 0">
             <div class="zg-flex zg-justify-center zg--mt-3 zg-w-full">
-                <a href="#" @click.prevent="addModule" class="zg-btn zg-btn-sm zg-bg-module">
+                <a href="#" @click.prevent="$emit('create')" class="zg-btn zg-btn-sm zg-bg-module">
                     <span class="icon-plus"></span>
                 </a>
             </div>
@@ -41,35 +41,7 @@ export default {
 
     props: {
         value: {}
-    },
-
-    methods: {
-        async addModule(index) {
-            this.openForm('module', 'Modul einfügen', {}).then(formData => {
-                var formData = { ...formData.data };
-                this.openForm(formData.is, formData.meta.title, formData.meta).then(moduleData => {
-                    formData.meta = moduleData;
-
-                    var content = this.value;
-                    content.splice(index+1, 0, formData);
-                    this.$emit('input', content);
-                }).catch(err => {
-                    console.log(err);
-                });
-            }).catch(err => {
-                console.log(err);
-            });
-        },
-        edit(module, index) {
-            this.openForm(module.is, module.meta.title, module.meta).then(moduleData => {
-                module.meta = moduleData;
-            }).catch(err => {
-                console.log(err);
-            });
-        },
-        remove(index) {
-            this.value.splice(index, 1);
-        }
     }
+
 };
 </script>
--- formwidgets/zgutenberg/assets/src/components/VRow.vue
+++ formwidgets/zgutenberg/assets/src/components/VRow.vue
@@ -2,7 +2,7 @@
     <div class="zg-p-4 zg-flex zg--mx-4">
 
         <div class="zg-p-4" :key="index" v-for="(column, index) in this.value" :class="'zg-w-'+column.width">
-            <v-column v-model="column.modules"></v-column>
+            <v-column v-model="column.modules" @create="createModule(index, $event)" @edit="editModule(index, $event)" @delete="deleteModule(index, $event)"></v-column>
         </div>
 
     </div>
@@ -10,11 +10,44 @@
 
 <script>
 import VColumn from './VColumn';
+import subform from '../subform.mixin.js';
 
 export default {
 
+    mixins: [ subform ],
+
     props: {
         value: {}
+    },
+
+    methods: {
+        createModule(index, moduleIndex) {
+            var modules = this.value[index].modules;
+
+            this.openForm('module', 'Modul einfügen', {}).then(formData => {
+                var formData = { ...formData.data };
+                this.openForm(formData.is, formData.meta.title, formData.meta).then(moduleData => {
+                    formData.meta = moduleData;
+
+                    modules.splice(moduleIndex+1, 0, formData);
+                }).catch(err => {
+                    console.log(err);
+                });
+            }).catch(err => {
+                console.log(err);
+            });
+        },
+        editModule(index, moduleIndex) {
+            var module = this.value[index].modules[moduleIndex];
+            this.openForm(module.is, module.meta.title, module.meta).then(moduleData => {
+                module.meta = moduleData;
+            }).catch(err => {
+                console.log(err);
+            });
+        },
+        deleteModule(index, moduleIndex) {
+            this.value[index].modules.splice(moduleIndex, 1);
+        }
     },
 
     components: { VColumn }
